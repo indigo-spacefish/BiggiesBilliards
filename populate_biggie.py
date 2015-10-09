6117554fb76682cc7ee3ff8fa02475d3821720c0
Ubuntu <ubuntu@ip-172-31-47-199.us-west-2.compute.internal>
2015-10-09 21:06:53 +0000

    new pop script

--- populate_biggie.py
+++ populate_biggie.py
@@ -13,14 +13,35 @@ def make_product(title):
     new_product[0].save()
     return new_product
 
-pool_cues = ['cue_1', 'cue_2', 'cue_3']
+pool_cues = ['JP01-M',
+'JP02-M',
+'JP03-M',
+'JP04-M',
+'JP05-M',
+'JP06-M',
+'JP07-M',
+'JP08-M',
+'JP09-M',
+'JP10-M',
+'JP11-M',
+'JP12-M',
+'JP13-M',
+'JP14-M',
+'JP15-M',
+'JP16-M',
+'JP17-M',
+'JP18-M',
+'JP19-M',
+'JP20-M',]
+
 
 def populate():
     for x in pool_cues:
-        make_product(x)
+        make_product('Pechauer JP Series(M)' + ' ' + x)
     print Product.objects.all()
 
 if __name__ == '__main__':
     print "Starting Biggies population script..."
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_site.settings')
     populate()
+

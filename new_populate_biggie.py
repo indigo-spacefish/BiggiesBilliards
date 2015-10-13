ad9044483f621d2ee1f0120f4495fec61b970b93
Ubuntu <ubuntu@ip-172-31-47-199.us-west-2.compute.internal>
2015-10-13 17:57:12 +0000

    pop script working add category

---
+++ new_populate_biggie.py
@@ -0,0 +1,70 @@
+import os
+from oscar.apps.catalogue.models import Product, ProductClass, ProductCategory, Category
+
+
+product_classes = ProductClass.objects.all()
+product_categories = Category.objects.all()
+pechauer_cat = product_categories[1]
+
+
+def make_product(title):
+    new_product = Product.objects.get_or_create(
+        title=title,
+        product_class=product_classes[0],
+    )
+
+    new_product[0].save()
+
+    new_cat = ProductCategory.objects.get_or_create(
+        product=new_product[0],
+        category=pechauer_cat
+    )
+
+    new_cat[0].save()
+
+    return new_product
+
+
+raw_data = '''Pro-H
+P02-F
+P03-F
+P04-F
+P05-F
+P06-F
+P07-F
+P08-F
+P09-F
+P10-F
+P11-F
+P12-F
+P13-F
+P14-F
+P15-F
+P16-F
+P17-F
+P18-F
+P19-F
+P20-F
+P21-F
+P22-F
+P23-F
+P24-F'''
+
+split_data = raw_data.split('\n')
+
+pool_cues = [i for i in split_data]
+
+
+
+def populate():
+    for x in pool_cues:
+        make_product('Pechauer JP Series(M)' + ' ' + x)
+    print Product.objects.all()
+
+if __name__ == '__main__':
+    print "Starting Biggies population script..."
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_site.settings')
+    populate()
+
+
+
